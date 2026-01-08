8536446dc5973cadce99bb8385194e40078a460e
teckydev <rs5916843@gmail.com>
2026-01-08 23:33:46 +0530

    config db

--- server/config/db.js
+++ server/config/db.js
@@ -2,13 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    const mongoURI =
-      process.env.NODE_ENV === 'production'
-        ? process.env.MONGO_ATLAS
-        : process.env.MONGO_URI;
-
-    await mongoose.connect(mongoURI);
-
+    await mongoose.connect(process.env.MONGO_URI);
     console.log('✅ MongoDB Connected');
   } catch (error) {
     console.error('❌ Error connecting to MongoDB:', error.message);
